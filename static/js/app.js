f8daf2d5ab7d1ec1a322620f0190c44202ed263d
Zachary <zach.gibbs@gmail.com>
2021-01-06 16:56:40 -0500

    Good progress

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 const dropdownMenu = d3.select("#selDataset");
 
 //Select the Demographic Box for later use
-const demoBox = d3.select("#panel-body");
+const demoBox = d3.select(".summary");
 
 //Read the data to get datasets
 d3.json("samples.json").then((readData) => {
@@ -14,69 +14,11 @@ d3.json("samples.json").then((readData) => {
   console.log(metadata);
 });
 
-
-// //Make a variable of the JSON file
-// const JSON = "samples.json";
-
-// //Convert the JSON to a readable state
-// const Data = d3.json(JSON, function(data) {
-//   console.log(data)
-// });
-
-// // Parse the JSON and make an array of ID's
-// const ID = d3.json(JSON).then(function(data) {
-//   console.log(data.names);
-// });
-
-// //Parse the JSON and make an array of Metadata
-// const Metadata = d3.json(JSON).then(function(data) {
-//   console.log(data.metadata);
-// });
-
-// //Parse the JSON and get the Sample data
-// const Samples = d3.json(JSON).then(function(data) {
-//   console.log(data.samples);
-// });
-// //Display an initial plot
-// function init() {
-//   //Choose initial ID for base data
-//   // var initial = Samples.id("940");
-// // Create the labels
-//   var labels = Object.keys(Samples.otu_ids === 940);
-//   console.log(labels);
-// //Choose initial dataset
-//   var dataset = Object.values(Samples.sample_values === 940);
-//   console.log(dataset);
-// //Create the plot data
-//   var trace1 = {
-//     x: dataset,
-//     y: labels,
-//     type: "bar"
-//   };
-  
-//   var data = [trace1]
-  
-//   var layout = {
-//     title: "Starter Graph",
-//     xaxis: {title: "Numbers"},
-//     yaxis: { title: "labels"}
-//     // height: 600,
-//     // width: 800
-//     // hovermode: 
-//   };
-  
-// Plotly.newPLot(".bar", data, layout);
-// };
-// function changeGraph() {
-//   let name = dropdownMenu.property("value");
-
-//   d3.json("samples.json")
-// }
-
 //Create initial state function
 const init = () => {
   //Append the ID's to the dropdown
   d3.json("samples.json").then(readData => {
+
     let data = readData;
     let fullID = data.names
     fullID.forEach(names => {
@@ -84,16 +26,86 @@ const init = () => {
     });
   });
   
-  var id = 940
+  var inputValue = 940
 
   //Read Input
-  console.log(id);
+  console.log(inputValue);
+
+  //Initial Demigraphics
+    //Try and input the Metadata
+    d3.json("samples.json").then(readData => {
+      let data = readData;
+      //Grab Metadata
+      let metadata = data.metadata;
+      console.log(metadata);
+      //Filter it down
+      let filteredMetadata = metadata.filter(metadata => metadata.id == inputValue);
+      console.log(filteredMetadata);
+      //Append a UL with the metadata
+      filteredMetadata.forEach(data => {
+        demoBox.append("li").text(`Age: ${data.age}`);
+        demoBox.append("li").text(`bbtype: ${data.bbtype}`);
+        demoBox.append("li").text(`Ethnicity: ${data.ethnicity}`);
+        demoBox.append("li").text(`Gender: ${data.gender}`);
+        demoBox.append("li").text(`location: ${data.location}`);
+      });
+    });
 }
 
 const optionChanged = () => {
   //grab the value
-  var id = dropdownMenu.property("value");
+  var inputValue = dropdownMenu.property("value");
+  // var inputValue = `${input}`
+  
   //Print the value
-  console.log(id);
+  console.log(inputValue);
+  
+  //Try and input the Metadata
+  d3.json("samples.json").then(readData => {
+    let data = readData;
+    //Grab Metadata
+    let metadata = data.metadata;
+    console.log(metadata);
+    //Filter it down
+    let filteredMetadata = metadata.filter(metadata => metadata.id == inputValue);
+    console.log(filteredMetadata);
+    //Clear demoBox
+    demoBox.html("");
+    
+    //Append a UL with the metadata
+    filteredMetadata.forEach(data => {
+      demoBox.append("li").text(`Age: ${data.age}`);
+      demoBox.append("li").text(`bbtype: ${data.bbtype}`);
+      demoBox.append("li").text(`Ethnicity: ${data.ethnicity}`);
+      demoBox.append("li").text(`Gender: ${data.gender}`);
+      demoBox.append("li").text(`location: ${data.location}`);
+    });
+  });
+  d3.json("samples.json").then(readData => {
+    let data = readData
+    let sampleData = data.samples;
+    console.log(sampleData);
+  });
 }
+
+// const metaData = () => {
+//     //Try and input the Metadata
+//     d3.json("samples.json").then(readData => {
+//       let data = readData;
+//       //Grab Metadata
+//       let metadata = data.metadata;
+//       console.log(metadata);
+//       //Filter it down
+//       let filteredMetadata = metadata.filter(metadata => metadata.id == inputValue);
+//       console.log(filteredMetadata);
+//       //Append a UL with the metadata
+//       filteredMetadata.forEach(data => {
+//         demoBox.append("li").text(`Age: ${data.age}`);
+//         demoBox.append("li").text(`bbtype: ${data.bbtype}`);
+//         demoBox.append("li").text(`Ethnicity: ${data.ethnicity}`);
+//         demoBox.append("li").text(`Gender: ${data.gender}`);
+//         demoBox.append("li").text(`location: ${data.location}`);
+//       });
+//     });
+// }
 init();
