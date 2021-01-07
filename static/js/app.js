7bc879a78668bd53a4bdf81f555ef8a908be8f7c
Zachary <zach.gibbs@gmail.com>
2021-01-06 23:06:20 -0500

    It's the Final COUNTDOWNgit add *!

--- static/js/app.js
+++ static/js/app.js
@@ -8,10 +8,8 @@ const demoBox = d3.select(".summary");
 d3.json("samples.json").then((readData) => {
   //Full dataset
   let data = readData;
-  console.log(data)
   //Metadata only
   let metadata = data.metadata;
-  console.log(metadata);
 });
 
 //Create initial state function
@@ -25,53 +23,22 @@ const init = () => {
       dropdownMenu.append("option").text(names).property("value", names);
     });
   });
-  
-  var inputValue = 940
-
-  //Read Input
-  console.log(inputValue);
-
-  //Initial Demigraphics
-    //Try and input the Metadata
-    d3.json("samples.json").then(readData => {
-      let data = readData;
-      //Grab Metadata
-      let metadata = data.metadata;
-      console.log(metadata);
-      //Filter it down
-      let filteredMetadata = metadata.filter(metadata => metadata.id == inputValue);
-      console.log(filteredMetadata);
-      //Append a UL with the metadata
-      filteredMetadata.forEach(data => {
-        demoBox.append("li").text(`Age: ${data.age}`);
-        demoBox.append("li").text(`bbtype: ${data.bbtype}`);
-        demoBox.append("li").text(`Ethnicity: ${data.ethnicity}`);
-        demoBox.append("li").text(`Gender: ${data.gender}`);
-        demoBox.append("li").text(`location: ${data.location}`);
-      });
-    });
 }
 
 const optionChanged = () => {
   //grab the value
   var inputValue = dropdownMenu.property("value");
-  // var inputValue = `${input}`
-  
   //Print the value
   console.log(inputValue);
-  
   //Try and input the Metadata
   d3.json("samples.json").then(readData => {
     let data = readData;
     //Grab Metadata
     let metadata = data.metadata;
-    console.log(metadata);
     //Filter it down
     let filteredMetadata = metadata.filter(metadata => metadata.id == inputValue);
-    console.log(filteredMetadata);
     //Clear demoBox
     demoBox.html("");
-    
     //Append a UL with the metadata
     filteredMetadata.forEach(data => {
       demoBox.append("li").text(`Age: ${data.age}`);
@@ -80,13 +47,62 @@ const optionChanged = () => {
       demoBox.append("li").text(`Gender: ${data.gender}`);
       demoBox.append("li").text(`location: ${data.location}`);
     });
-  });
-  d3.json("samples.json").then(readData => {
-    let data = readData
+    //Gather Sample Data
     let sampleData = data.samples;
-    console.log(sampleData);
+    let filteredSampleData = sampleData.filter(sampleData => sampleData.id == inputValue);
+    let slicedSampleNames = filteredSampleData[0].otu_ids.slice(0,10);
+    let stringSampleNames = slicedSampleNames.map(sample => `OTU ${sample}`);
+    console.log(stringSampleNames);
+    let slicedSampleValues = filteredSampleData[0].sample_values.slice(0,10);
+    // // //Order values
+    // // let orderedSampleValues = filteredSampleData.map(object => object.sample_values)//.sort((a,b) => a-b);
+    // // Slice down to size
+    // let slicedSampleValues = orderedSampleValues.map(object => object.slice(0,10));
+    // //Grab Sample names
+    // let filteredSampleNames = filteredSampleData.map(object => object.otu_ids);
+    // //Slice down to size
+    // let slicedSampleNames = filteredSampleNames.map(object => object.slice(0,10));
+    // console.log(slicedSampleNames);
+    // console.log(slicedSampleValues);
+
+    let trace1 =  {
+      type: "bar",
+      x: slicedSampleValues,
+      y: stringSampleNames,
+      orientation: "h"
+    };
+    let graphData = [trace1];
+    let layout = {
+      title: "Top 10 Bacteria Found",
+      yaxis: {
+        tickmode:"array",
+        tickvals: slicedSampleNames
+        }
+      };
+    
+    Plotly.newPlot("bar", graphData, layout);
   });
 }
+
+  
+  
+  
+  // //Create the Bar chart
+  // d3.json("samples.json").then(readData => {
+  //   let data = readData
+  //   let sampleData = data.samples;
+  //   //Filter Sample Data
+  //   let filteredSampleData = sampleData.filter(sampleData => sampleData.id == inputValue);
+  //   console.log(filteredSampleData);
+  //   let Values = filteredSampleData.otu_ids;
+  //   console.log(Values)
+
+  
+    //Slice down the data
+    // let values = filteredSampleData.sample_values;
+    // console.log(values);
+//   });
+// }
 
 // const metaData = () => {
 //     //Try and input the Metadata
